5f69aaf7c65304094202b574b01b0cdb600353d4
shisq <shhz1994@163.com>
2021-04-02 10:15:52 +0800

    初始化

---
+++ src/test/java/study/springboot2/StudyJavaSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJavaSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
